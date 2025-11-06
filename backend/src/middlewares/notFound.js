04d1565064c708b96b1270177405ded624ee7f57
Björka Skarsgård <adam.bastian_ti24@nusaputra.ac.id>
2025-11-06 19:44:32 +0700

    Delete backend directory

--- backend/src/middlewares/notFound.js
+++
@@ -1,9 +0,0 @@
-// /backend/src/middlewares/notFound.js
-
-const notFound = (req, res, next) => {
-  const error = new Error(`Tidak ditemukan - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
-
-module.exports = { notFound };
